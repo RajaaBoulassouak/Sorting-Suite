4826046228bce2979556f051df819124c96272c3
RajaaBoulassouak <raj.b.stretz@gmail.com>
2019-01-21 01:48:21 -0700

    bubble sort complete

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,12 +1,14 @@
-var array = [2,4,5,1,3]
+var array = [2,4,5,1,3];
 
 function bubbleSort(array) {
-  var i, neighbor, limit 
+  var i, neighbor, limit;
   limit = array.length; while(limit--) {
     for(i = 0, neighbor = 1; i < limit; ++i, ++neighbor)
       if(array[i] > array[neighbor]) {temporary = array[i]; array[i] = array[neighbor]; array[neighbor] = temporary;}
-  }
-  return array
-}
+  };
+  return array;
+};
 
-module.exports = bubbleSort
+console.log(bubbleSort(array));
+
+module.exports = bubbleSort;
